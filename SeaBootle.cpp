4dd0ca311f822e6c6bacc93c8e0909f85c0267e6
BardinValentin <46860651+BardinValentin@users.noreply.github.com>
2019-04-07 09:00:54 +0300

    Update SeaBootle.cpp

--- SeaBootle.cpp
+++ SeaBootle.cpp
@@ -1,14 +1,9 @@
 ﻿//Игра морской бой. Выполнили: Фролов Д.В. , Бардин В.С. . Версия 1.0
 
 #include "pch.h"
-#pragma comment(lib, "ws2_32.lib")
-#include <winsock2.h>
 #include <iostream>
-#include <sstream>
-#include <fstream>
-#include <cstdlib>
-#include <string>
-#include <stdlib.h>
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
 #pragma warning(disable: 4996)
 
 using namespace std;
@@ -17,307 +12,21 @@ int corX, corY, winValue;
 int fieldPlayer[N][N];
 int fieldComp[N][N];
 SOCKET Connection;
+
 void showEmptyField(int numberArray);
+
+void enterLinkor();
+void enterCruiser();
+void enterDestroyer();
+void enterBoat();
+
+int ClientHandler();
+
+int shoting();
+
+void chekWounded();
+
 int winGame();
-
-void enterLinkor()
-{
-	showEmptyField(1);
-enter:
-	cout << "Enter linkor coordinates(4 cells):" << endl;
-	int X, Y, A, B, X1, Y1, A1, B1, h;
-	do
-	{
-		cout << "Enter the first coordinate of the horizontal:"; cin >> X; cout << endl;
-		cout << "Enter the first vertical coordinate:"; cin >> Y; cout << endl;
-	} while (X < 1 || X > 10 || Y < 1 || Y>10);
-enter1:
-	do {
-		cout << "Enter the second horizontal coordinate:"; cin >> A; cout << endl;
-		cout << "Enter the second vertical coordinate:"; cin >> B; cout << endl;
-	} while (A < 1 || A>10 || B < 1 || B>10);
-	h = abs((X + Y) - (A + B));
-	if (h != 1)
-	{
-		cout << "You have entered the coordinates of a neighboring square." << endl;
-		goto enter1;
-	}
-enter2:
-	do
-	{
-		cout << "Enter the third horizontal coordinate:"; cin >> X1; cout << endl;
-		cout << "Enter the third vertical coordinate:"; cin >> Y1; cout << endl;
-	} while (X1 < 1 || X1 > 10 || Y1 < 1 || Y1>10);
-	h = abs((X1 + Y1) - (A + B));
-	if (h != 1)
-	{
-		cout << "You have entered the coordinates of a neighboring square." << endl;
-		goto enter2;
-	}
-enter3:
-	do {
-		cout << "Enter the fourth horizontal coordinate:"; cin >> A1; cout << endl;
-		cout << "Enter the fourth vertical coordinate:"; cin >> B1; cout << endl;
-	} while (A1 < 1 || A1>10 || B1 < 1 || B1>10);
-	h = abs((X1 + Y1) - (A1 + B1)); 
-	if (h != 1)
-	{
-		cout << "You have entered the coordinates of a neighboring square." << endl;
-		goto enter3;
-	}
-	if (((X == X1) && (X1 == A) && (A == A1)) || ((Y == Y1) && (Y1 == B) && ( B == B1)))
-	{
-		fieldPlayer[A1 - 1][B1 - 1] = 1;
-		fieldPlayer[A - 1][B - 1] = 1;
-		fieldPlayer[X - 1][Y - 1] = 1;
-		fieldPlayer[X1 - 1][Y1 - 1] = 1;
-	}
-	else
-	{
-		goto enter;
-	}
-	system("cls");
-	showEmptyField(1);
-}
-
-void enterCruiser()
-{
-	showEmptyField(1);
-enter:
-	cout << "Enter the coordinates of the cruiser(3 cells):" << endl;
-	int X, Y, A, B, X1, Y1, h;
-	do
-	{
-		cout << "Enter the first coordinate of the horizontal:"; cin >> X; cout << endl;
-		cout << "Enter the first vertical coordinate:"; cin >> Y; cout << endl;
-	} while (X < 1 || X > 10 || Y < 1 || Y>10);
-enter1:
-	do {
-		cout << "Enter the second horizontal coordinate:"; cin >> A; cout << endl;
-		cout << "Enter the second vertical coordinate:"; cin >> B; cout << endl;
-	} while (A < 1 || A>10 || B < 1 || B>10);
-	h = abs((X + Y) - (A + B));
-	if (h != 1)
-	{
-		cout << "You have entered the coordinates of a neighboring square." << endl;
-		goto enter1;
-	}
-enter2:
-	do
-	{
-		cout << "Enter the third horizontal coordinate:"; cin >> X1; cout << endl;
-		cout << "Enter the third vertical coordinate:"; cin >> Y1; cout << endl;
-	} while (X1 < 1 || X1 > 10 || Y1 < 1 || Y1>10);
-	h = abs((X1 + Y1) - (A + B));
-	if (h != 1)
-	{
-		cout << "You have entered the coordinates of a neighboring square." << endl;
-		goto enter2;
-	}
-	if (((X == X1) && (X1 == A)) || ((Y == Y1) && (Y1 == B)))
-	{
-		fieldPlayer[A - 1][B - 1] = 1;
-		fieldPlayer[X - 1][Y - 1] = 1;
-		fieldPlayer[X1 - 1][Y1 - 1] = 1;
-	}
-	else
-	{
-		goto enter;
-	}
-	system("cls");
-	showEmptyField(1);
-}
-
-void enterDestroyer()
-{
-	cout << "Enter the coordinates of the destroyer(2 cells):" << endl;
-	int X, Y, A, B, h;
-enter:
-	do
-	{
-		cout << "Enter the first coordinate of the horizontal:"; cin >> X; cout << endl;
-		cout << "Enter the first vertical coordinate:"; cin >> Y; cout << endl;
-	} while (X < 1 || X > 10 || Y < 1 || Y>10);
-	if (fieldPlayer[X][Y] == 1 || fieldPlayer[X - 1][Y] == 1 || fieldPlayer[X - 2][Y] == 1 || fieldPlayer[X][Y - 1] == 1 || fieldPlayer[X][Y - 2] == 1 || fieldPlayer[X - 1][Y - 2] == 1 || fieldPlayer[X - 2][Y - 2] == 1 || fieldPlayer[X - 2][Y - 1] == 1)
-	{
-		goto enter;
-	}
-	if (fieldPlayer[X - 1][Y - 1] != 0)
-	{
-		goto enter;
-	}
-
-enter1:
-	do {
-		cout << "Enter the second horizontal coordinate:"; cin >> A; cout << endl;
-		cout << "Enter the second vertical coordinate:"; cin >> B; cout << endl;
-	} while (A < 1 || A>10 || B < 1 || B>10);
-	h = abs((X + Y) - (A + B));
-	if (h != 1)
-	{
-		cout << "You have entered the coordinates of a neighboring square." << endl;
-		goto enter1;
-	}
-	if (fieldPlayer[A][B] == 1 || fieldPlayer[A - 1][B] == 1 || fieldPlayer[A - 2][B] == 1 || fieldPlayer[A][B - 1] == 1 || fieldPlayer[A][B - 2] == 1 || fieldPlayer[A - 1][B - 2] == 1 || fieldPlayer[A - 2][B - 2] == 1 || fieldPlayer[A - 2][B - 1] == 1)
-	{
-		goto enter1;
-	}
-	if (fieldPlayer[A - 1][B - 1] == 0)
-	{
-		fieldPlayer[A - 1][B - 1] = 1;
-	}
-	else
-	{
-		goto enter1;
-	}
-	fieldPlayer[X - 1][Y - 1] = 1;
-	system("cls");
-	showEmptyField(1);
-}
-
-void enterBoat()
-{
-	cout << "Enter the coordinates of the boat(1 cells):" << endl;
-	int X, Y, h;
-enter:
-	do
-	{
-		cout << "Enter the first coordinate of the horizontal:"; cin >> X; cout << endl;
-		cout << "Enter the first vertical coordinate:"; cin >> Y; cout << endl;
-	} while (X < 1 || X > 10 || Y < 1 || Y>10);
-	if (fieldPlayer[X][Y] == 1 || fieldPlayer[X - 1][Y] == 1 || fieldPlayer[X - 2][Y] == 1 || fieldPlayer[X][Y - 1] == 1 || fieldPlayer[X][Y - 2] == 1 || fieldPlayer[X - 1][Y - 2] == 1 || fieldPlayer[X - 2][Y - 2] == 1 || fieldPlayer[X - 2][Y - 1] == 1)
-	{
-		goto enter;
-	}
-	if (fieldPlayer[X - 1][Y - 1] == 0)
-	{
-		fieldPlayer[X - 1][Y - 1] = 1;
-	}
-	else
-	{
-		goto enter;
-	}
-	system("cls");
-	showEmptyField(1);
-}
-
-int ClientHandler()
-{
-	char msg1[256];
-	char msg[256];
-	recv(Connection, msg, sizeof(msg), NULL);
-	int X, Y;
-	X = (int)msg[0];
-	Y = (int)msg[1];
-	if (fieldPlayer[X - 1][Y - 1] == 0)
-	{
-		fieldPlayer[X - 1][Y - 1] = 2;
-	}
-	if (fieldPlayer[X - 1][Y - 1] == 1)
-	{
-		if (fieldPlayer[X - 2][Y - 2] == 1 || fieldPlayer[X - 2][Y - 1] == 1 || fieldPlayer[X - 2][Y] == 1 || fieldPlayer[X - 1][Y - 2] == 1 || fieldPlayer[X - 1][Y] == 1 || fieldPlayer[X][Y - 2] == 1 || fieldPlayer[X][Y - 1] == 1 || fieldPlayer[X][Y] == 1)
-		{
-			fieldPlayer[X - 1][Y - 1] = 3;
-		}
-	}
-	if (fieldPlayer[X - 1][Y - 1] == 1)
-	{
-		if (fieldPlayer[X - 2][Y - 2] != 1 || fieldPlayer[X - 2][Y - 1] != 1 || fieldPlayer[X - 2][Y] != 1 || fieldPlayer[X - 1][Y - 2] != 1 || fieldPlayer[X - 1][Y] != 1 || fieldPlayer[X][Y - 2] != 1 || fieldPlayer[X][Y - 1] != 1 || fieldPlayer[X][Y] != 1)
-		{
-			fieldPlayer[X - 1][Y - 1] = 4;
-		}
-	}
-	msg1[0] = (char)fieldPlayer[X - 1][Y - 1];
-	send(Connection, msg1, sizeof(msg1), NULL);
-	system("cls");
-	showEmptyField(1);
-	showEmptyField(2);
-	return fieldPlayer[X - 1][Y - 1];
-}
-
-int shoting()
-{
-	showEmptyField(1);
-	showEmptyField(2);
-	char msg1[256];
-	cout << "Enter the coordinates for shooting" << endl;
-	cout << "Vertical coordinate"; cin >> corX; cout << endl;
-	cout << "Horizontal coordinate"; cin >> corY; cout << endl;
-	msg1[0] = (char)corX;
-	msg1[1] = (char)corY;
-	send(Connection, msg1, sizeof(msg1), NULL);
-	Sleep(10);
-
-	char msg[256];
-	recv(Connection, msg, sizeof(msg), NULL);
-	fieldComp[corX - 1][corY - 1] = (int)msg[0];
-	system("cls");
-	showEmptyField(1);
-	showEmptyField(2);
-	return fieldComp[corX - 1][corY - 1];
-}
-
-void chekWounded()
-{
-	for (int b = 0; b < 4; b++)
-	{
-		for (int i = 0; i < 10; i++)
-		{
-			for (int j = 0; j < 10; j++)
-			{
-				if (fieldPlayer[i][j] == 4)
-				{
-					if (fieldPlayer[i - 1][j] == 3)
-					{
-						fieldPlayer[i - 1][j] = 4;
-					}
-					if (fieldPlayer[i + 1][j] == 3)
-					{
-						fieldPlayer[i + 1][j] = 4;
-					}
-					if (fieldPlayer[i][j - 1] == 3)
-					{
-						fieldPlayer[i][j - 1] = 4;
-					}
-					if (fieldPlayer[i][j + 1] == 3)
-					{
-						fieldPlayer[i][j + 1] = 4;
-					}
-
-				}
-
-			}
-		}
-	}
-	for (int b = 0; b < 4; b++)
-	{
-		for (int i = 0; i < 10; i++)
-		{
-			for (int j = 0; j < 10; j++)
-			{
-				if (fieldComp[i][j] == 4)
-				{
-					if (fieldComp[i - 1][j] == 3)
-					{
-						fieldComp[i - 1][j] = 4;
-					}
-					if (fieldComp[i + 1][j] == 3)
-					{
-						fieldComp[i + 1][j] = 4;
-					}
-					if (fieldComp[i][j - 1] == 3)
-					{
-						fieldComp[i][j - 1] = 4;
-					}
-					if (fieldComp[i][j + 1] == 3)
-					{
-						fieldComp[i][j + 1] = 4;
-					}
-				}
-			}
-		}
-	}
-}
 
 int main()
 {
@@ -599,6 +308,305 @@ void showEmptyField(int numberArray)
 		std::cout << (char)205;
 	}
 	std::cout << (char)188 << endl;
+}
+
+void enterLinkor()
+{
+	showEmptyField(1);
+enter:
+	cout << "Enter linkor coordinates(4 cells):" << endl;
+	int X, Y, A, B, X1, Y1, A1, B1, h;
+	do
+	{
+		cout << "Enter the first coordinate of the horizontal:"; cin >> X; cout << endl;
+		cout << "Enter the first vertical coordinate:"; cin >> Y; cout << endl;
+	} while (X < 1 || X > 10 || Y < 1 || Y>10);
+enter1:
+	do {
+		cout << "Enter the second horizontal coordinate:"; cin >> A; cout << endl;
+		cout << "Enter the second vertical coordinate:"; cin >> B; cout << endl;
+	} while (A < 1 || A>10 || B < 1 || B>10);
+	h = abs((X + Y) - (A + B));
+	if (h != 1)
+	{
+		cout << "You have entered the coordinates of a neighboring square." << endl;
+		goto enter1;
+	}
+enter2:
+	do
+	{
+		cout << "Enter the third horizontal coordinate:"; cin >> X1; cout << endl;
+		cout << "Enter the third vertical coordinate:"; cin >> Y1; cout << endl;
+	} while (X1 < 1 || X1 > 10 || Y1 < 1 || Y1>10);
+	h = abs((X1 + Y1) - (A + B));
+	if (h != 1)
+	{
+		cout << "You have entered the coordinates of a neighboring square." << endl;
+		goto enter2;
+	}
+enter3:
+	do {
+		cout << "Enter the fourth horizontal coordinate:"; cin >> A1; cout << endl;
+		cout << "Enter the fourth vertical coordinate:"; cin >> B1; cout << endl;
+	} while (A1 < 1 || A1>10 || B1 < 1 || B1>10);
+	h = abs((X1 + Y1) - (A1 + B1));
+	if (h != 1)
+	{
+		cout << "You have entered the coordinates of a neighboring square." << endl;
+		goto enter3;
+	}
+	if (((X == X1) && (X1 == A) && (A == A1)) || ((Y == Y1) && (Y1 == B) && (B == B1)))
+	{
+		fieldPlayer[A1 - 1][B1 - 1] = 1;
+		fieldPlayer[A - 1][B - 1] = 1;
+		fieldPlayer[X - 1][Y - 1] = 1;
+		fieldPlayer[X1 - 1][Y1 - 1] = 1;
+	}
+	else
+	{
+		goto enter;
+	}
+	system("cls");
+	showEmptyField(1);
+}
+
+void enterCruiser()
+{
+	showEmptyField(1);
+enter:
+	cout << "Enter the coordinates of the cruiser(3 cells):" << endl;
+	int X, Y, A, B, X1, Y1, h;
+	do
+	{
+		cout << "Enter the first coordinate of the horizontal:"; cin >> X; cout << endl;
+		cout << "Enter the first vertical coordinate:"; cin >> Y; cout << endl;
+	} while (X < 1 || X > 10 || Y < 1 || Y>10);
+enter1:
+	do {
+		cout << "Enter the second horizontal coordinate:"; cin >> A; cout << endl;
+		cout << "Enter the second vertical coordinate:"; cin >> B; cout << endl;
+	} while (A < 1 || A>10 || B < 1 || B>10);
+	h = abs((X + Y) - (A + B));
+	if (h != 1)
+	{
+		cout << "You have entered the coordinates of a neighboring square." << endl;
+		goto enter1;
+	}
+enter2:
+	do
+	{
+		cout << "Enter the third horizontal coordinate:"; cin >> X1; cout << endl;
+		cout << "Enter the third vertical coordinate:"; cin >> Y1; cout << endl;
+	} while (X1 < 1 || X1 > 10 || Y1 < 1 || Y1>10);
+	h = abs((X1 + Y1) - (A + B));
+	if (h != 1)
+	{
+		cout << "You have entered the coordinates of a neighboring square." << endl;
+		goto enter2;
+	}
+	if (((X == X1) && (X1 == A)) || ((Y == Y1) && (Y1 == B)))
+	{
+		fieldPlayer[A - 1][B - 1] = 1;
+		fieldPlayer[X - 1][Y - 1] = 1;
+		fieldPlayer[X1 - 1][Y1 - 1] = 1;
+	}
+	else
+	{
+		goto enter;
+	}
+	system("cls");
+	showEmptyField(1);
+}
+
+void enterDestroyer()
+{
+	cout << "Enter the coordinates of the destroyer(2 cells):" << endl;
+	int X, Y, A, B, h;
+enter:
+	do
+	{
+		cout << "Enter the first coordinate of the horizontal:"; cin >> X; cout << endl;
+		cout << "Enter the first vertical coordinate:"; cin >> Y; cout << endl;
+	} while (X < 1 || X > 10 || Y < 1 || Y>10);
+	if (fieldPlayer[X][Y] == 1 || fieldPlayer[X - 1][Y] == 1 || fieldPlayer[X - 2][Y] == 1 || fieldPlayer[X][Y - 1] == 1 || fieldPlayer[X][Y - 2] == 1 || fieldPlayer[X - 1][Y - 2] == 1 || fieldPlayer[X - 2][Y - 2] == 1 || fieldPlayer[X - 2][Y - 1] == 1)
+	{
+		goto enter;
+	}
+	if (fieldPlayer[X - 1][Y - 1] != 0)
+	{
+		goto enter;
+	}
+
+enter1:
+	do {
+		cout << "Enter the second horizontal coordinate:"; cin >> A; cout << endl;
+		cout << "Enter the second vertical coordinate:"; cin >> B; cout << endl;
+	} while (A < 1 || A>10 || B < 1 || B>10);
+	h = abs((X + Y) - (A + B));
+	if (h != 1)
+	{
+		cout << "You have entered the coordinates of a neighboring square." << endl;
+		goto enter1;
+	}
+	if (fieldPlayer[A][B] == 1 || fieldPlayer[A - 1][B] == 1 || fieldPlayer[A - 2][B] == 1 || fieldPlayer[A][B - 1] == 1 || fieldPlayer[A][B - 2] == 1 || fieldPlayer[A - 1][B - 2] == 1 || fieldPlayer[A - 2][B - 2] == 1 || fieldPlayer[A - 2][B - 1] == 1)
+	{
+		goto enter1;
+	}
+	if (fieldPlayer[A - 1][B - 1] == 0)
+	{
+		fieldPlayer[A - 1][B - 1] = 1;
+	}
+	else
+	{
+		goto enter1;
+	}
+	fieldPlayer[X - 1][Y - 1] = 1;
+	system("cls");
+	showEmptyField(1);
+}
+
+void enterBoat()
+{
+	cout << "Enter the coordinates of the boat(1 cells):" << endl;
+	int X, Y, h;
+enter:
+	do
+	{
+		cout << "Enter the first coordinate of the horizontal:"; cin >> X; cout << endl;
+		cout << "Enter the first vertical coordinate:"; cin >> Y; cout << endl;
+	} while (X < 1 || X > 10 || Y < 1 || Y>10);
+	if (fieldPlayer[X][Y] == 1 || fieldPlayer[X - 1][Y] == 1 || fieldPlayer[X - 2][Y] == 1 || fieldPlayer[X][Y - 1] == 1 || fieldPlayer[X][Y - 2] == 1 || fieldPlayer[X - 1][Y - 2] == 1 || fieldPlayer[X - 2][Y - 2] == 1 || fieldPlayer[X - 2][Y - 1] == 1)
+	{
+		goto enter;
+	}
+	if (fieldPlayer[X - 1][Y - 1] == 0)
+	{
+		fieldPlayer[X - 1][Y - 1] = 1;
+	}
+	else
+	{
+		goto enter;
+	}
+	system("cls");
+	showEmptyField(1);
+}
+
+int ClientHandler()
+{
+	char msg1[256];
+	char msg[256];
+	recv(Connection, msg, sizeof(msg), NULL);
+	int X, Y;
+	X = (int)msg[0];
+	Y = (int)msg[1];
+	if (fieldPlayer[X - 1][Y - 1] == 0)
+	{
+		fieldPlayer[X - 1][Y - 1] = 2;
+	}
+	if (fieldPlayer[X - 1][Y - 1] == 1)
+	{
+		if (fieldPlayer[X - 2][Y - 2] == 1 || fieldPlayer[X - 2][Y - 1] == 1 || fieldPlayer[X - 2][Y] == 1 || fieldPlayer[X - 1][Y - 2] == 1 || fieldPlayer[X - 1][Y] == 1 || fieldPlayer[X][Y - 2] == 1 || fieldPlayer[X][Y - 1] == 1 || fieldPlayer[X][Y] == 1)
+		{
+			fieldPlayer[X - 1][Y - 1] = 3;
+		}
+	}
+	if (fieldPlayer[X - 1][Y - 1] == 1)
+	{
+		if (fieldPlayer[X - 2][Y - 2] != 1 || fieldPlayer[X - 2][Y - 1] != 1 || fieldPlayer[X - 2][Y] != 1 || fieldPlayer[X - 1][Y - 2] != 1 || fieldPlayer[X - 1][Y] != 1 || fieldPlayer[X][Y - 2] != 1 || fieldPlayer[X][Y - 1] != 1 || fieldPlayer[X][Y] != 1)
+		{
+			fieldPlayer[X - 1][Y - 1] = 4;
+		}
+	}
+	msg1[0] = (char)fieldPlayer[X - 1][Y - 1];
+	send(Connection, msg1, sizeof(msg1), NULL);
+	system("cls");
+	showEmptyField(1);
+	showEmptyField(2);
+	return fieldPlayer[X - 1][Y - 1];
+}
+
+int shoting()
+{
+	showEmptyField(1);
+	showEmptyField(2);
+	char msg1[256];
+	cout << "Enter the coordinates for shooting" << endl;
+	cout << "Vertical coordinate"; cin >> corX; cout << endl;
+	cout << "Horizontal coordinate"; cin >> corY; cout << endl;
+	msg1[0] = (char)corX;
+	msg1[1] = (char)corY;
+	send(Connection, msg1, sizeof(msg1), NULL);
+	Sleep(10);
+
+	char msg[256];
+	recv(Connection, msg, sizeof(msg), NULL);
+	fieldComp[corX - 1][corY - 1] = (int)msg[0];
+	system("cls");
+	showEmptyField(1);
+	showEmptyField(2);
+	return fieldComp[corX - 1][corY - 1];
+}
+
+void chekWounded()
+{
+	for (int b = 0; b < 4; b++)
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			for (int j = 0; j < 10; j++)
+			{
+				if (fieldPlayer[i][j] == 4)
+				{
+					if (fieldPlayer[i - 1][j] == 3)
+					{
+						fieldPlayer[i - 1][j] = 4;
+					}
+					if (fieldPlayer[i + 1][j] == 3)
+					{
+						fieldPlayer[i + 1][j] = 4;
+					}
+					if (fieldPlayer[i][j - 1] == 3)
+					{
+						fieldPlayer[i][j - 1] = 4;
+					}
+					if (fieldPlayer[i][j + 1] == 3)
+					{
+						fieldPlayer[i][j + 1] = 4;
+					}
+
+				}
+
+			}
+		}
+	}
+	for (int b = 0; b < 4; b++)
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			for (int j = 0; j < 10; j++)
+			{
+				if (fieldComp[i][j] == 4)
+				{
+					if (fieldComp[i - 1][j] == 3)
+					{
+						fieldComp[i - 1][j] = 4;
+					}
+					if (fieldComp[i + 1][j] == 3)
+					{
+						fieldComp[i + 1][j] = 4;
+					}
+					if (fieldComp[i][j - 1] == 3)
+					{
+						fieldComp[i][j - 1] = 4;
+					}
+					if (fieldComp[i][j + 1] == 3)
+					{
+						fieldComp[i][j + 1] = 4;
+					}
+				}
+			}
+		}
+	}
 }
 
 int winGame()
